a4a6148ec60a18f64ad2052bad2b043525029390
Fredrik Eriksson <eriksson.nf@gmail.com>
2021-12-13 11:43:43 +0100

    final changes before turning in assignment

--- src/main/java/org/example/AthleteDao.java
+++ src/main/java/org/example/AthleteDao.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public interface AthleteDao {
 
-    void create(Athlete athlete);
     Athlete getById(int id);
+    void create(Athlete athlete);
     void delete(Athlete athlete);
     List<Athlete> getByTeam(String team);
     List<Athlete> getByName(String name);
